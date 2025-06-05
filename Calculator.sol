e795e05329b92d05eaffd3dfd30e90e72e963ee3
K.venkata saikrishna <153519012+saikrishna406@users.noreply.github.com>
2025-06-05 23:07:47 +0530

    Update Calculator.sol

--- Calculator.sol
+++ Calculator.sol
@@ -2,18 +2,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract Calculator{
-    uint256  result = 0;
+    uint  result = 0;
     
-    function add(uint256 num) public {
+    function add(uint num) public {
         result +=num;
     }
-    function subtract (uint256 num)public{
+    function subtract (uint num)public{
         result -=num;
     }
-    function multiplication (uint256 num)public {
+    function multiplication (uint num)public {
         result *=num;
     }
-    function division (uint256 num)public {
+    function division (uint num)public {
         result /=num;
     }
     function get() public view returns (uint256)
